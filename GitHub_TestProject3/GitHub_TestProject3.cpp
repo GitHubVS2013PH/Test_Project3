b5811c14eb84702b7b1ecb6521c624f79f82ce6e
GitHubVS2013PH <phayter1@gmail.com>
2015-10-16 15:43:40 -0700

    Initial Commit for Project 3

---
+++ GitHub_TestProject3/GitHub_TestProject3.cpp
@@ -0,0 +1,12 @@
+// GitHub_TestProject3.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   cout << "Hello World. Testing GitHub with a new Project (Project #3)!\n";
+   return 0;
+}
+
